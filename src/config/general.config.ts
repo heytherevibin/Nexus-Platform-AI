fe1f12965b7dfc6dcce51514276ce28310a200c4
heytherevibin <ebin1995@gmail.com>
2025-05-09 23:00:32 +0100

    Update general configuration with new documentation and support links
    - Replaced existing links with updated URLs for documentation, status, support, and terms.

--- src/config/general.config.ts
+++ src/config/general.config.ts
@@ -1,8 +1,8 @@
 const generalSettings: any = {
-  purchaseLink: 'https://1.envato.market/Vm7VRE',
-  devsLink: 'https://devs.keenthemes.com',
-  faqLink: 'https://keenthemes.com/metronic',
-  aboutLink: 'https://keenthemes.com/metronic'
+  docsUrl: 'https://yourapp.com/docs',
+  statusUrl: 'https://status.yourapp.com',
+  supportUrl: 'https://yourapp.com/support',
+  termsUrl: 'https://yourapp.com/terms'
 };
 
 export { generalSettings };
